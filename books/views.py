8dcaf5c260040a60f4466e9e515ec24b95113383
kiran45kumar <kirankiccha2003@gmail.com>
2025-07-20 19:54:15 +0530

    Add CRUD views for Book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create(request):
+    return render(request, 'books/create.html')
+def read(request):
+    return render(request, 'books/read.html')
+def update(request):
+    return render(request, 'books/update.html') 
+def delete(request):
+    return render(request, 'books/delete.html')
